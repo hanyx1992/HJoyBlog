11305f339824e4570b68d3263e9c038d533f0101
hanyx1992@sina.com <hanyx1992@sina.com>
2015-12-28 17:21:11 +0800

    后台初步搭建及权限校验

---
+++ src/com/hanyx/hjoyblog/controller/SysAdminController.java
@@ -0,0 +1,27 @@
+package com.hanyx.hjoyblog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 后台管理页面
+ * @author 韩元旭
+ */
+@Controller
+@RequestMapping("/admin")
+public class SysAdminController {
+	
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
+	@RequestMapping(value = "/index.do")
+	public ModelAndView toHomePage() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("hello", mongoTemplate.getDb());
+		mv.setViewName("/admin/setting");
+		return mv;
+    }
+}
---
+++ src/com/hanyx/hjoyblog/filter/SysAdminAuthFilter.java
@@ -0,0 +1,26 @@
+package com.hanyx.hjoyblog.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class SysAdminAuthFilter extends OncePerRequestFilter{
+
+	private static Log LOGGER = LogFactory.getLog(SysAdminAuthFilter.class);
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request,
+			HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		LOGGER.info("进入校验登录的filter...");
+		filterChain.doFilter(request, response);
+	}
+
+}
