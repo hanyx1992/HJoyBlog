9ba5baf3f8d0374516d1dbc0a139c4d3493f27fd
hanyx1992@sina.com <hanyx1992@sina.com>
2016-01-11 18:05:45 +0800

    用户信息初始化

---
+++ src/com/hanyx/hjoyblog/test/UserInit.java
@@ -0,0 +1,53 @@
+package com.hanyx.hjoyblog.test;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.hanyx.hjoyblog.bean.User;
+import com.hanyx.hjoyblog.dao.UserDao;
+import com.hanyx.hjoyblog.util.GlobalConstraints;
+import com.hanyx.hjoyblog.util.StringUtil;
+
+/**
+ * @desc:向User用户表中插入初始化数据
+ * @author 韩元旭
+ */
+@RunWith(SpringJUnit4ClassRunner.class) 
+@ContextConfiguration(locations = {"file:WebContent/WEB-INF/spring-servlet.xml"})
+public class UserInit{
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	@Autowired
+	private UserDao userDao;
+	
+	@Test
+	public void Test() {
+		this.init();
+		System.out.println(userDao.queryById("hanyx").getNickName());
+	}
+	
+	public void init() {
+		//清空集合
+		mongoTemplate.dropCollection(User.class);
+		
+		//初始化数据
+		User u = new User();
+		u.setLoginName("hanyx");
+		u.setLoginPwd(StringUtil.encryptByMD5("123456"));
+		u.setNickName("旭丶Joy");
+		u.setRole(GlobalConstraints.USER_ROLE_ID_ADMINISTRATOR);
+		u.setTel("13803648899");
+		u.setEmail("hanyx1992@sina.com");
+		u.setIsUsed(GlobalConstraints.BEAN_DATA_STATE_ISUSED);
+		u.setCreateDate(new Date());
+		userDao.save(u);
+	}
+	
+}
